531ac619cba1692a579a19ff2dfa2b6405b7ba60
denis436 <dgeorgiev436>
2022-07-21 01:47:07 +0000

    Props type with NativeStackHeaderProps

--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,7 +1,8 @@
 import { View, Text, Button } from "react-native"
+import { NativeStackHeaderProps } from "@react-navigation/native-stack"
 
-export default function PlannerScreen ({navigation}: any) {
-	
+export default function PlannerScreen ({navigation}: NativeStackHeaderProps) {
+
 	return(
 		<View>
 			<Text>I am planner screen</Text>
